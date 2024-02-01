2568f96b7f6372990c6ae6aac8e5288db5a2bbbd
kassem00 <kamsdonga@gmail.com>
2024-02-01 20:43:29 +0000

    au

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_is_leaf - check if node is leaf.
  * @node: A pointer to the noode to be checked.
- * Return: 1 if mode is leaf else 0 if NULL return 0.
+ * Return: 1 if node is leaf else 0 if NULL return 0.
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
